c7aeb33d9a9e811f3243521b61e436da62951f3a
shixiaojionga <156561927+shixiaojionga@users.noreply.github.com>
2024-01-20 00:16:50 +0800

    Update main.c

--- 2-1 OLED显示屏/main.c
+++ 2-1 OLED显示屏/main.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 	OLED_Init();
-	OLED_ShowString(1, 1, "Wang Shenglong,");
+	OLED_ShowString(1, 1, "xxxxx,");
 	OLED_ShowString(2, 1, "good good study,");
 	OLED_ShowString(3, 1, "day day up!");
 	while(1)
